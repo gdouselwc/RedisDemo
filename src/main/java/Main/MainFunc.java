f3ad4a4bfa37647423130c75b7021bb901de33bc
gdouselwc <15361093829@163.com>
2019-08-03 23:16:15 +0800

    xxx;

--- src/main/java/Main/MainFunc.java
+++ src/main/java/Main/MainFunc.java
@@ -11,14 +11,14 @@ public class MainFunc {
     public static void main(String[] args){
 
         UserModel userModel = new UserModel();
-        userModel.setUserId("80246303");
+        userModel.setUserId("123");
         userModel.setUserName("hello");
-        userModel.setPhone("18138447785");
+        userModel.setPhone("123456");
 
         RedisDao redisDao = new RedisDao("192.168.0.108",6379);
-        //redisDao.SetUser2Redis(userModel);
+        redisDao.SetUser2Redis(userModel);
 
-        UserModel result = redisDao.GetUser("80246303");
+        UserModel result = redisDao.GetUser("123");
         System.out.println(result.toString());
     }
 }
